9c268723e69a227ccf59a131461d502ddac2defc
sam <samuelmcowan22@gmail.com>
2020-11-26 22:11:06 +0000

    Initial main menu and controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
 public class Controller {
+    private GameBoard board;
+
+    public Controller(){
+        board = new GameBoard();
+    }
+
+    public void runGame(){
+
+    }
+
+    public void minimaxAB(){
+
+    }
+
+    public int evaluate(GameBoard board){
+
+        return 0;
+    }
 }
---
+++ src/sample/MainMenu.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+    @Override
+    public void start(Stage mainStage) throws Exception{
+
+    }
+}
